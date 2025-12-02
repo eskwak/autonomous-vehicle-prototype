a06973486a64022f6fdbe6dbae9514443e6905c2
Eddie <ekwak30@gatech.edu>
2025-12-02 14:59:22 -0500

    repo initialization

---
+++ Vehicle Firmware/src/main.cpp
@@ -0,0 +1,20 @@
+/**
+ *      Description:      Contains firmware setup and main loop.
+ * 
+ *      Author:           Eddie Kwak
+ *      Last Modified:    12/2/2025
+ */
+
+
+#include <Arduino.h>
+
+
+// Setup function: Contains single-execution firmware.
+void setup(void) {
+
+}
+
+// Main loop: contains repeated execution firmware.
+void loop(void) {
+    
+}
